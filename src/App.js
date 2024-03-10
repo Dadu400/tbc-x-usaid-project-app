a9659d78a88c9b17bba3dacd83f129d3e79da0a5
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-03-10 18:38:13 +0400

    Finished footer without responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <Footer />
     </div>
   );
 }
