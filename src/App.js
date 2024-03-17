e8a7c4041e2c24f1c8ca8ef7948db419bd17f576
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-03-18 02:44:52 +0400

    Added new section to app and changed css styles for no x scroll

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Banner from "./components/banner/Banner";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import ProductList from "./components/products/ProductList";
 import "./index.css"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col h-screen">
       <Header />
-      <Banner />
+      {/* <Banner /> */}
+      <ProductList />
       <Footer />
     </div>
   );
