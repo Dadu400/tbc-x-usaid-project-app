67aee12b7e744fe4aef64f6b918cb302db1b8214
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-03-26 15:04:09 +0400

    Changed logo image directory

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LegoLogo from "../icons/LEGO_logo.svg.png";
+import LegoLogo from "../icons/LEGO_logo.png";
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -20,7 +20,7 @@ function Header() {
                             <Link to="/">shop</Link>
                         </li>
                         <li className="uppercase font-bold text-base">
-                            <Link to="/">discover</Link>
+                            <Link to="/profile">profile</Link>
                         </li>
                         <li className="uppercase font-bold text-base">
                             <Link to="/Contacts">contact us</Link>
