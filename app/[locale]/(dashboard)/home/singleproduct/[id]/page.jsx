709378d93c14fbe31857daa5b893ec7bdae17396
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-04-26 12:37:08 +0400

    Finished translation on the single product page

--- app/[locale]/(dashboard)/home/singleproduct/[id]/page.jsx
+++ app/[locale]/(dashboard)/home/singleproduct/[id]/page.jsx
@@ -2,6 +2,7 @@ import { getSingleProduct } from "@/app/helpers/axiosProduct";
 import Image from "next/image";
 import axios from "axios";
 import DashboardLayout from "../../../DashboardLayout";
+import { getTranslations } from "next-intl/server";
 
 export async function generateStaticParams() {
   const res = await axios.get("https://dummyjson.com/products/category/groceries");
@@ -11,6 +12,7 @@ export async function generateStaticParams() {
 }
 
 export default async function singleProduct({ params }) {
+  const t = await getTranslations("singleProduct")
   const idString = params?.id;
   const id = Number(idString);
   const product = await getSingleProduct(id);
@@ -18,7 +20,7 @@ export default async function singleProduct({ params }) {
   return (
     <DashboardLayout>
     <section className="w-full">
-      <div className="w-full min-h-[440px] flex items-center justify-center gap-6 py-10 px-8 shadow-xl dark:bg-black">
+      <div className="w-full min-h-[646px] flex items-center justify-center gap-6 py-10 px-8 shadow-xl dark:bg-black">
         <div className="flex justify-center w-2/4">
           <Image
             src={product.images[0]}
@@ -33,25 +35,25 @@ export default async function singleProduct({ params }) {
             {product.title}
           </p>
           <p className="text-lg text-gray-700 dark:text-white">
-            <span className="font-semibold">Brand:</span> {product.brand}
+            <span className="font-semibold">{t("brand")}:</span> {product.brand}
           </p>
           <p className="text-md text-gray-600 mt-4 dark:text-white">
-            <span className="font-semibold">Description:</span>{" "}
+            <span className="font-semibold">{t("description")}:</span>{" "}
             {product.description}
           </p>
           <p className="text-md text-gray-600 dark:text-white">
-            <span className="font-semibold">Category:</span> {product.category}
+            <span className="font-semibold">{t("category")}:</span> {product.category}
           </p>
           <p className="text-md text-gray-600 dark:text-white">
-            <span className="font-semibold">Stock:</span>{" "}
+            <span className="font-semibold">{t("stock")}:</span>{" "}
             {product.stock > 0 ? "In stock" : "Out of stock"}
           </p>
           <p className="text-2xl text-gray-900 font-semibold mt-4 dark:text-white">
-            <span className="font-semibold">Price:</span> ${product.price}
+            <span className="font-semibold">{t("price")}:</span> ${product.price}
           </p>
           <p className="text-md text-yellow-500 mt-2">
             {" "}
-            <span className="font-semibold">Rating:</span>
+            <span className="font-semibold">{t("rating")}:</span>
             {Array(Math.max(0, Math.min(5, Math.floor(product.rating || 0))))
               .fill()
               .map((_, i) => "⭐")}
