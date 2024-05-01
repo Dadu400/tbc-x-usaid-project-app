9d4c89ff8afecf593eef8be939efb6fa64dadf77
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-05-01 18:12:55 +0400

    Added internationalization to the login page

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -5,6 +5,7 @@ import { AUTH_COOKIE_KEY } from "../../../contants";
 import { Login } from "../../actions";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import LocaleSwitcher from "../../../components/header/LocaleSwitcher";
 
 export default async function LoginPage() {
   const cookiesStore = cookies();
@@ -20,7 +21,10 @@ export default async function LoginPage() {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100 dark:bg-black">
+    <div className="flex flex-col justify-center items-center h-screen bg-[#FD8024] dark:bg-black">
+      <div className="mb-2">
+      <LocaleSwitcher />
+      </div>
       <LoginForm handleLogin={handleLogin} />
     </div>
   );
--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React, { useState } from 'react';
+import { useLocale } from "next-intl";
 
 interface LoginFormProps {
   handleLogin: (email: string, password: string) => void;
@@ -9,6 +10,7 @@ interface LoginFormProps {
 function LoginForm({ handleLogin }: LoginFormProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const locale = useLocale();
 
   const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -20,7 +22,7 @@ function LoginForm({ handleLogin }: LoginFormProps) {
       <form autoComplete='off' className="flex flex-col space-y-4">
         <div>
           <label htmlFor="email" className="block text-sm font-medium text-gray-700 dark:text-white">
-            Email
+          {locale == "en" ? "Username" : "მომხარებლის სახელი"}
           </label>
           <input
             type="email"
@@ -34,7 +36,7 @@ function LoginForm({ handleLogin }: LoginFormProps) {
         </div>
         <div>
           <label htmlFor="password" className="block text-sm font-medium text-gray-700 dark:text-white">
-            Password
+          {locale == "en" ? "Password" : "მომხარებლის პაროლი"}
           </label>
           <input
             type="password"
@@ -50,7 +52,7 @@ function LoginForm({ handleLogin }: LoginFormProps) {
           onClick={handleSubmit}
           className="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         >
-          Log In
+          {locale == "en" ? "Log In" : "შესვლა"}
         </button>
       </form>
     </div>
