6836cf214aa6fbc1a077811304de2b4481296d9c
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-05-02 00:21:16 +0400

    Added unstable_setRequestLocale on dashboard layouts

--- app/[locale]/(dashboard)/DashboardLayout.tsx
+++ app/[locale]/(dashboard)/DashboardLayout.tsx
@@ -3,20 +3,18 @@ import Footer from "../../../components/footer/Footer";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { AUTH_COOKIE_KEY } from "../../../contants";
-import { useLocale } from "next-intl";
 import { ReactNode } from "react";
 
 interface DashboardLayoutProps {
   children: ReactNode;
 }
 
-function DashboardLayout({ children }: DashboardLayoutProps) {
+function DashboardLayout({ children}: DashboardLayoutProps) {
   const cookieStore = cookies();
-  const locale = useLocale();
   const cookie = cookieStore.get(AUTH_COOKIE_KEY);
 
   if (!cookie?.value) {
-    return redirect(`${locale}/login`)
+    return redirect("/login")
   }
 
   return (
