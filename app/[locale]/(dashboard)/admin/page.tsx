5f929694732861d1d4a9bd6ecfcd850b58a7f168
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-05-09 23:14:39 +0400

    Added admin panel to the app route

--- app/[locale]/(dashboard)/admin/page.tsx
+++ app/[locale]/(dashboard)/admin/page.tsx
@@ -1,60 +1,10 @@
-import { getUsers, User } from "../../../../helpers/axiosUsers";
 import DashboardLayout from "../DashboardLayout";
+import UsersTable from "../../../../components/admin/UsersTable";
 
 export default async function UsersPage() {
-  const users = await getUsers();
   return (
     <DashboardLayout>
-      <div className="max-w-5xl mx-auto mt-14 min-h-[380px]">
-        <table className="min-w-full divide-y divide-gray-200 shadow-md">
-          <thead className="bg-gray-50">
-            <tr>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                ID
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Name
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Email
-              </th>
-              <th
-                scope="col"
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                Age
-              </th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {users.map((user: User) => (
-              <tr key={user.id}>
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                  {user.id}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                  {user.name}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                  {user.email}
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                  {user.age}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+        <UsersTable />
     </DashboardLayout>
   );
 }
