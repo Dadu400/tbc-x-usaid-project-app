7a4f0f201238f65157aef7ffa3c0934c411f2195
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-05-16 16:55:17 +0400

    finished editing user component with its functionality

--- actions.ts
+++ actions.ts
@@ -2,7 +2,8 @@
 
 import { cookies } from "next/headers";
 import { AUTH_COOKIE_KEY } from "./contants";
-import { createUser, deleteUser } from "./helpers/axiosUsers";
+import { createUser, deleteUser, updateUser } from "./helpers/axiosUsers";
+import { revalidateTag } from "next/cache";
 
 export async function Login(username: string, password: string) {
   const response = await fetch("https://dummyjson.com/auth/login", {
@@ -33,8 +34,14 @@ export async function Logout() {
 export async function createUserAction(formData: FormData) {
   const { name, email, age } = Object.fromEntries(formData);
   createUser(name as string, email as string, age as string);
+  revalidateTag("users_list");
 }
 
 export async function deleteUserAction(id: number) {
   await deleteUser(id);
+}
+
+export async function updateUserAction(formData: FormData) {
+  const { id, name, email, age } = Object.fromEntries(formData);
+  updateUser(id as string, name as string, email as string, age as string)
 }
