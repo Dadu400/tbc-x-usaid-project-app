a0d2997aa780035afc1342f463b352d0a65acfaa
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-05-17 16:55:53 +0400

    Update BASE_URL and environment configuration

--- helpers/getEnvironment.ts
+++ helpers/getEnvironment.ts
@@ -1,8 +1,8 @@
 export function getEnvironment() {
   const enviro = process.env.NODE_ENV;
   if (enviro === "production") {
-    return "https://tbc-accelerator-project.vercel.app/";
+    return "https://tbc-x-usaid-project-app.vercel.app";
   } else {
-    return "http://localhost:3000/";
+    return "http://localhost:3000";
   }
 }
