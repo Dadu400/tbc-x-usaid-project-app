b692d303056fe00b9dd1ea1faa7eda8b5fe082dd
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-06-01 01:14:09 +0400

    registration UI finished

--- components/registration/UserRegistrationForm.tsx
+++ components/registration/UserRegistrationForm.tsx
@@ -1,45 +1,89 @@
-import { useTranslations } from "next-intl";
+"use client";
+
+import Image from "next/image";
+import DefaultImage from "../../public/person.png";
+import { useState } from "react";
 
 const UserRegistrationForm = () => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]; // Add a null check for event.target.files
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file));
+    }
+  };
 
-  const t = useTranslations("Register");
   return (
-    <div className="px-8 pt-6 pb-8 bg-white rounded shadow-2xl dark:bg-black">
+    <div className="px-8 pt-6 pb-8 bg-white rounded shadow-2xl">
       <h2 className="text-xl font-['mtavruli'] font-semibold mb-[20px] text-center w-full">რეგისტრაცია</h2>
-      <form autoComplete='off' className="flex flex-col space-y-4">
-        <div className='w-[350px]'>
-          <label htmlFor="email" className="block text-md font-medium text-gray-700 dark:text-white">
-            მომხარებლის სახელი
+      <Image src={selectedImage || DefaultImage} id="registrationImage" alt="user" width={120} height={30} className="mx-auto rounded-full border border-2 cursor-pointer w-[120px] h-[120px]" />
+      <form autoComplete='off' className="flex flex-col space-y-4 mt-[10px]">
+        <div className='w-full'>
+          <label htmlFor="image" className="block font-medium text-gray-700 dark:text-white">
+            პროფილის სურათი
           </label>
           <input
-            type="email"
-            id="email"
-            name="email"
+            type="file"
+            id="image"
+            name="image"
+            accept="image/*"
             required
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:red sm:text-sm"
-          // value={email}
-          // onChange={(e) => setEmail(e.target.value)}
+            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring-red focus:border-red focus:outline-none focus:red"
+            onChange={handleImageChange}
+            placeholder="პროფილის სურათი"
           />
         </div>
-        <div>
-          <label htmlFor="password" className="block text-md font-medium text-gray-700 dark:text-white">
-            მომხარებლის პაროლი
-          </label>
+        <div className="h-[2px] bg-gradient-to-r from-white via-red to-white"> </div>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="ელ. ფოსტა"
+          required
+          className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring-red focus:border-red focus:outline-none focus:red"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="პაროლი"
+          required
+          className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring-red focus:border-red focus:outline-none focus:red"
+        />
+        <div className="flex items-center justify-between">
           <input
-            type="password"
-            id="password"
+            type="text"
+            id="name"
+            placeholder="სახელი"
             required
-            className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          // value={password}
-          // onChange={(e) => setPassword(e.target.value)}
+            className="w-[40%] px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring-red focus:border-red focus:outline-none focus:red"
+          />
+          <input
+            type="text"
+            id="surname"
+            placeholder="გვარი"
+            required
+            className="w-[56%] px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring-red focus:border-red focus:outline-none focus:red"
           />
         </div>
+        <input
+          type="text"
+          id="address"
+          placeholder="მისამართი"
+          required
+          className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring-red focus:border-red focus:outline-none focus:red"
+        />
+        <input
+          type="text"
+          id="phone"
+          placeholder="ტელეფონი"
+          required
+          className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:ring-red focus:border-red focus:outline-none focus:red"
+        />
+
         <button
           type="submit"
-          // onClick={handleSubmit}
-          className="w-full px-4 py-2 text-md font-medium text-white bg-red rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 "
-        >
+          className="w-full px-4 py-2 text-md font-medium text-white bg-red rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2">
           რეგისტრაცია
         </button>
       </form>
