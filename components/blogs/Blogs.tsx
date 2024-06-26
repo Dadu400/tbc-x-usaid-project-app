0c81e101f3713a180d0b52e2df0706718e47e32a
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-06-27 03:23:48 +0400

    added countup in carts page

--- components/blogs/Blogs.tsx
+++ components/blogs/Blogs.tsx
@@ -1,15 +1,19 @@
 import { GetSession, getBlogs } from "../../actions";
 import BlogCard, { Blog } from "./BlogCard";
+import { getTranslations } from "next-intl/server";
+import localFont from '@next/font/local';
 
+const mtavruli = localFont({ src: '../../public/fonts/mtavruli.ttf' })
 
 export default async function Blogs() {
+  const t = await getTranslations("Blog");
   const session = await GetSession();
 
   const fetchedBlogs: Blog[] = await getBlogs();
 
   return (
     <section className="w-[60vw] m-auto flex flex-col my-[35px]">
-      <div className="text-center text-2xl font-semibold font-['mtavruli'] mb-[35px]">სიახლეები</div>
+      <div className={`${mtavruli.className} text-center text-2xl font-semibold mb-[35px] uppercase`}>{t("header")}</div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 bg-[#F9F9FF] dark:bg-[#121B18] mb-[150px]">
         {fetchedBlogs.map((blog: Blog) => (
           <BlogCard key={blog.id} session={session} blog={blog} />
